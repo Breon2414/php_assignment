4285eb49fb57e87bee8e8a307277e878f17b0a67
Breon2414 <42629280+Breon2414@users.noreply.github.com>
2018-10-29 23:40:30 -0400

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -1,8 +1,10 @@
 <html>
 <body>
 
-Welcome <?php echo $_POST["name"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?>
+Welcome <php echo $_POST["name"]; ?><br>
+Your email address is: <php echo $_POST["email"];
+Your location is: <php echo $_POST["location"];
+Your subject is: <php echo $_POST["subject"];?>
 
 </body>
 </html>
